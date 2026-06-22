2b0e918239aa7c6f867a2858ad3a05bf69690b82
Fyp07 <felyppe9253@gmail.com>
2026-06-22 18:01:01 +0100

    save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fbarrada <fbarrada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 17:06:54 by fbarrada          #+#    #+#             */
-/*   Updated: 2026/06/22 14:23:34 by fbarrada         ###   ########.fr       */
+/*   Updated: 2026/06/22 17:37:24 by fbarrada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,26 @@ int main(int argc, char **argv)
 {
 	t_input input;
 	t_list *stack_a;
+	t_list *stack_b;
 	char **str;
-	int	i;
 	
-	i = 0;
+	stack_b = NULL;
 	if (argc < 2)
 		return (0);
 	str = argv_to_string(argc - 1, argv + 1); // Faz o seguinte: 1 2 "3 4 5" 6 7; -> 1 2 3 4 5 6 7;
 	if (processing_args(str, &input)) // Verifica se os argumentos sao validos
 		return (write(2, "Error\n", 6), free_split(str), 1); // Se houver um erro, retorna uma mensagem de erro, da free em tudo e retorna 1);
-	stack_a = create_stack(str, input.start);
+	stack_a = create_stack(str, input.start); // Cria a stack;
 	if (!stack_a)
 		return(write(2, "Error\n", 6), free_split(str), 1);
-	print_stack(stack_a, "A");
-	ft_printf("\n");
+	print_stack(stack_a, "A"); // Printa a stack;
 	ra(&stack_a);
 	print_stack(stack_a, "A");
+	sa(&stack_a);
+	print_stack(stack_a, "A");
+	pb(&stack_b, stack_a);
+	print_stack(stack_b, "B");
+	
+
 	return (free_split(str), 0);
 }
---
+++ operations.c
@@ -0,0 +1,183 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbarrada <fbarrada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 16:39:50 by fbarrada          #+#    #+#             */
+/*   Updated: 2026/06/22 17:27:54 by fbarrada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "./Libft/libft.h"
+
+void	sa(t_list **lista_a)
+{
+	t_list	*head;
+	t_list	*node2;
+	t_list	*node3;
+	
+	if (!lista_a || !(*lista_a)->next)
+		return ;
+		
+	// Atribuindo valores
+	head = *lista_a;
+	node2 = head->next;
+	node3 = node2->next;
+
+	// Troca
+	head->next = node3;
+	head->previous = node2;
+	
+	node2->next = head;
+	node2->previous = NULL;
+
+	node3->previous = head;
+
+	*lista_a = node2;
+}
+
+void	sb(t_list **lista_b)
+{
+	t_list	*head;
+	t_list	*node2;
+	t_list	*node3;
+	
+	if (!lista_b || !(*lista_b)->next)
+		return ;
+		
+	// Atribuindo valores
+	head = *lista_b;
+	node2 = head->next;
+	node3 = node2->next;
+
+	// Troca
+	head->next = node3;
+	head->previous = node2;
+	
+	node2->next = head;
+	node2->previous = NULL;
+
+	node3->previous = head;
+
+	*lista_b = node2;
+}
+
+void	ss(t_list **lista_a, t_list **lista_b)
+{
+	sa(lista_a);
+	sb(lista_b);
+}
+
+void	pa(t_list **lista_a, t_list *node_b)
+{
+	t_list *temp;
+	t_list *head_a;
+	
+	if (!node_b)
+		return ;
+
+	temp = node_b->next;
+	if ((*lista_a)->next)
+	{
+		head_a = *lista_a;
+		ft_lstadd_front(lista_a, node_b);
+		node_b = temp;
+		head_a->previous = *lista_a;
+		(*lista_a)->next = head_a;
+	}
+	else
+	{
+		ft_lstadd_front(lista_a, node_b);
+		node_b = temp;
+	}
+}
+
+void	pb(t_list **lista_b, t_list *node_a)
+{
+	t_list *temp;
+	t_list *head_b;
+	
+	if (!node_a)
+		return ;
+
+	temp = node_a->next;
+	if ((*lista_b)->next)
+	{
+		head_b = *lista_b;
+		ft_lstadd_front(lista_b, node_a);
+		node_a = temp;
+		head_b->previous = *lista_b;
+		(*lista_b)->next = head_b;
+	}
+	else
+	{
+		ft_lstadd_front(lista_b, node_a);
+		node_a = temp;
+	}
+}
+
+void	ra(t_list **lista_a)
+{
+	t_list *head;
+	t_list *last;
+	t_list *temp;
+	
+	head = *lista_a; // Define a cabeca da lista
+	last = ft_lstlast(*lista_a); // Define o final da lista
+	temp = head->next;
+	
+	last->next = head;
+	head->next = NULL;
+	head->previous = last;
+	head->previous = last;
+	
+	temp->previous = NULL;
+	*lista_a = temp;
+}
+
+void	rb(t_list **lista_b)
+{
+	t_list *head;
+	t_list *last;
+	t_list *temp;
+	
+	head = *lista_b; // Define a cabeca da lista
+	last = ft_lstlast(*lista_b); // Define o final da lista
+	temp = head->next;
+	
+	last->next = head;
+	head->next = NULL;
+	head->previous = last;
+	head->previous = last;
+	
+	temp->previous = NULL;
+	*lista_b = temp;
+}
+
+void	rr(t_list **lista_b, t_list **lista_a)
+{
+	ra(lista_a);
+	rb(lista_b);
+}
+
+// void	rra(t_list **lista_a)
+// {
+// 	t_list *last;
+	
+// 	last = ft_lstlast(*lista_a);
+// 	ft_lstadd_front(lista_a, last);
+// }
+
+// void	rrb(t_list **lista_b)
+// {
+// 	rra(lista_b);
+// }
+
+// void	rrr(t_list **lista_a, t_list **lista_b)
+// {
+// 	rra(lista_a);
+// 	rrb(lista_b);
+// }
